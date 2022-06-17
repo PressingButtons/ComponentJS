8479dd4e1d48abd6452ab48b721ad148ae4ff140
Steven Boyd <20269544+PressingButtons@users.noreply.github.com>
2022-06-17 17:43:34 -0500

    creating extration package for use.

---
+++ main.js
@@ -0,0 +1,13 @@
+import Component from './component.js';
+import Panel from './presets/panel.js';
+import Table from './presets/table.js';
+
+Object.defineProperty(window, "ComponentLib", {value: { }});
+
+ Object.defineProperties(ComponentLib, {
+   Component: {value: Component},
+   preset: {value: Object.defineProperties({}, {
+     Panel: {value: Panel},
+     Table: {value: Table}
+   });
+})
--- presets/table.js
+++ presets/table.js
@@ -9,20 +9,6 @@ export default class TableComponent extends Component {
     this.setTable(options);
   }
 
-  #setHeader(headers) {
-    if(!headers) throw "Error: TableComponent requires an array of headers in configuration";
-    this.header.removeChildren( );
-    for(const tag of headers) {
-      const th = this.header.addComponent(tag, 'th');
-      th.write(tag);
-    }
-  }
-
-  #setBody(n) {
-    if(!n || n < 1) n = 1;
-    this.content.removeChildren( );
-    for(let i = 0; i < n; i++) this.addRow( );
-  }
 
   addRow( ) {
     const tr = this.content.addComponent(`row:${this.content.body.rows.length}`, {type: 'tr'});
@@ -32,9 +18,24 @@ export default class TableComponent extends Component {
     }
   }
 
+  setBody(n) {
+    if(!n || n < 1) n = 1;
+    this.content.removeChildren( );
+    for(let i = 0; i < n; i++) this.addRow( );
+  }
+
+  setHeader(headers) {
+    if(!headers) throw "Error: TableComponent requires an array of headers in configuration";
+    this.header.removeChildren( );
+    for(const tag of headers) {
+      const th = this.header.addComponent(tag, 'th');
+      th.write(tag);
+    }
+  }
+
   setTable(options) {
-    this.#setHeader(options.headers);
-    this.#setBody(options.rows);
+    this.setHeader(options.headers);
+    this.setBody(options.rows);
   }
 
 }
