0494bdac1a2b1cbc06a775826b6d2bf9f017ccc9
Steven Boyd <20269544+PressingButtons@users.noreply.github.com>
2022-06-17 19:31:30 -0500

    patch: panel. Panel did not write name on initiation.

--- presets/panel.js
+++ presets/panel.js
@@ -6,6 +6,7 @@ export default class PanelComponent extends Component {
     super(name, {type: 'div', style: "display:flex; flex-flow: column nowrap;", className: "panel_component"});
     this.addComponent('header', {type: 'div', style: "display: flex; justify-content: space-between;", className: "panel_header"});
     this.header.addComponent('title', {type: 'h1', className: "panel_title"});
+    this.header.title.write(name);
     this.header.addComponent('ui', {type: 'div', style: 'display: flex'});
     this.header.ui.addComponent('close', {type: "p", className: "panel_close"});
     this.addComponent('content', {type: "div", style: "flex: 1; padding: 6px;", className: "panel_content"});
