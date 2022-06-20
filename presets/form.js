55e7903c1cb397fbafdd83710a131e1f523d356a
Steven Boyd <20269544+PressingButtons@users.noreply.github.com>
2022-06-20 18:23:06 -0500

    adding form preset

---
+++ presets/form.js
@@ -0,0 +1,24 @@
+import Component from '../component.js';
+
+export default class FormComponent extends Component {
+
+  constructor(name) {
+    super(name, {className: "form_component", style: "display:flex; flex-flow: column nowrap;"});
+    this.addComponent('header', {className: "form_header", type: "h1"});
+    this.header.write(name);
+    this.addComponent("form", {type: 'form', {className: "form_form"}});
+    this.form.addComponent('content', {className: "form_component"});
+    this.form.addComponent("buttons", {className: "form_buttons"});
+    this.form.buttons.addComponent("submit", {type: "button", attributes: {type: "submit"}, className: "form_submit"});
+    this.sections =  {};
+  }
+
+  addSection(name, content) {
+    const section = this.form.contnet.addComponent(name, {className: "form_section"});
+    this.sections[name] = section;
+    section.addComponent('header', {type: 'h1', className: "header"});
+    section.header.write(name);
+    section.addSection('content', {className: "content"});
+  }
+
+}
