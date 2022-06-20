46c6955d54d249ac3f5f969e893227478d6fe835
Steven Boyd <20269544+PressingButtons@users.noreply.github.com>
2022-06-20 18:25:44 -0500

    placing form into main

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 import Component from './component.js';
 import Panel from './presets/panel.js';
 import Table from './presets/table.js';
+import Form from './presets/form.js';
 
 Object.defineProperty(window, "ComponentLib", {value: { }});
 
  Object.defineProperties(ComponentLib, {
    Component: {value: Component},
    Panel: {value: Panel},
-   Table: {value: Table}
+   Table: {value: Table},
+   Form: {value: Form}
 })
